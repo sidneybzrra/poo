fdaf504d8c48c27c33d2635f63ace7c5d6c3626a
ELIÉVERTON DANTAS <elieverton.dantas@dce.ufpb.br>
2015-12-01 10:36:54 -0300

    atualizado

--- ProjetoPoo/Disciplina.java
+++ ProjetoPoo/Disciplina.java
@@ -93,6 +93,31 @@ public class Disciplina {
 		}
 		return teste;
 	}
-	
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Disciplina other = (Disciplina) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+   
+
 }
+
